b0eb6851960eee9cafd5e85327a451c0f610ed85
Nokuvimba <G00423951@atu.ie>
2024-04-10 16:27:10 +0100

    the addTest passed

--- src/main/java/ie/atu/calc/Calculator.java
+++ src/main/java/ie/atu/calc/Calculator.java
@@ -1,9 +1,10 @@
-package ie.atu;
+package ie.atu.calc;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-
+public class Calculator {
+    public int add(int first,int second){
+        return (first +second);
     }
+
 }
